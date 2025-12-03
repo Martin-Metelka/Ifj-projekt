76de6e46b11bb0a6f4992b997a69717607ed6fda
Martin-Metelka <martin.metelka1@seznam.cz>
2025-12-04 00:33:50 +0100

    Update symtable.h

--- symtable.h
+++ symtable.h
@@ -1,3 +1,14 @@
+/**
+ * Project: Implementace překladače jazyka IFJ25.
+ *
+ * symtable.h
+ * VBS
+ *
+ * @author Jakub Gono - xgonoja00
+ * @author Vojtěch Kabelka - xkabelv00
+ * @author Filip Kachyňa - xkachyf00
+ * @author Martin Metelka - xmetelm00
+ */
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
 
